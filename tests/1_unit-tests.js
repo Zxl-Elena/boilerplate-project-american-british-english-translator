e8c925156802b3692d70c34b4367289158e53043
Zxl-Elena <146520668+Zxl-Elena@users.noreply.github.com>
2025-05-09 00:03:00 +0000

    complete translation logic and unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -60,5 +60,69 @@ suite('Unit Tests', () => {
         const expected = 'We watched the <span class="highlight">soccer</span> match for a while.';
         assert.equal(translator.britishToAmerican(text), expected);
     })
-    
+    test('Translate Paracetamol takes up to an hour to work. to American English', () => {
+        const text = "Paracetamol takes up to an hour to work.";
+        const expected = '<span class="highlight">Tylenol</span> takes up to an hour to work.';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Translate First, caramelise the onions. to American English', () => {
+        const text = "First, caramelise the onions.";
+        const expected = 'First, <span class="highlight">caramelize</span> the onions.';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Translate I spent the bank holiday at the funfair. to American English', () => {
+        const text = "I spent the bank holiday at the funfair.";
+        const expected = 'I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Translate I had a bicky then went to the chippy. to American English', () => {
+        const text = "I had a bicky then went to the chippy.";
+        const expected = 'I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test("Translate I've just got bits and bobs in my bum bag. to American English", () => {
+        const text = "I've just got bits and bobs in my bum bag.";
+        const expected = "I've just got <span class=\"highlight\">odds and ends</span> in my <span class=\"highlight\">fanny pack</span>.";
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Translate The car boot sale at Boxted Airfield was called off. to American English', () => {
+        const text = "The car boot sale at Boxted Airfield was called off.";
+        const expected = 'The <span class=\"highlight\">swap meet</span> at Boxted Airfield was called off.';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Translate Have you met Mrs Kalyani? to American English', () => {
+        const text = "Have you met Mrs Kalyani?";
+        const expected = 'Have you met <span class="highlight">Mrs.</span> Kalyani?';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Translate Prof Joyner of King\'s College, London. to American English', () => {
+        const text = "Prof Joyner of King's College, London.";
+        const expected = "<span class=\"highlight\">Prof.</span> Joyner of King's College, London.";
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Translate Tea time is usually around 4 or 4.30. to American English', () => {
+        const text = "Tea time is usually around 4 or 4.30.";
+        const expected = 'Tea time is usually around 4 or <span class="highlight">4:30</span>.';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Highlight translation in Mangoes are my favorite fruit.', () => {
+        const text = "Mangoes are my favorite fruit.";
+        const expected = 'Mangoes are my <span class="highlight">favourite</span> fruit.';
+        assert.equal(translator.americanToBritish(text), expected);
+    })
+    test('Highlight translation in I ate yogurt for breakfast.', () => {
+        const text = "I ate yogurt for breakfast.";
+        const expected = 'I ate <span class="highlight">yoghurt</span> for breakfast.';
+        assert.equal(translator.americanToBritish(text), expected);
+    })
+    test('Highlight translation in We watched the footie match for a while.', () => {
+        const text = "We watched the footie match for a while.";
+        const expected = 'We watched the <span class="highlight">soccer</span> match for a while.';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
+    test('Highlight translation in Paracetamol takes up to an hour to work.', () => {
+        const text = "Paracetamol takes up to an hour to work.";
+        const expected = '<span class="highlight">Tylenol</span> takes up to an hour to work.';
+        assert.equal(translator.britishToAmerican(text), expected);
+    })
 });
