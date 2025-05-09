6ef733ce91fab06230a5c34f2cd7c36f6d604f15
Zxl-Elena <146520668+Zxl-Elena@users.noreply.github.com>
2025-05-09 01:16:11 +0000

    Implement API logic for /api/translate

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,22 @@ module.exports = function (app) {
 
       if (!text || !locale) { return res.json({ error: 'Required field(s) missing' })}
       if (text.trim() === '') { return res.json({ error: 'No text to translate' })};
-      if (!locale === 'americanToBritish' || !locale === 'britishToAmerican') { return res.json({ error: 'Invalid value for locale field' })};
+      if (locale !== 'americanToBritish' || locale !== 'britishToAmerican') { return res.json({ error: 'Invalid value for locale field' })};
       
-      
+      if (locale === 'americanToBritish') {
+        const translatedResult = translator.americanToBritish(text);
+        if (translatedResult.replaced === false) {
+          return res.json({text: "Everything looks good to me!"});
+        }
+        return res.json({text: translatedResult.text});
+      }
+
+      if (locale === 'britishToAmerican') {
+        const translatedResult = translator.britishToAmerican(text);
+        if (translatedResult.replaced === false) {
+          return res.json({text: "Everything looks good to me!"});
+        }
+        return res.json({text: translatedResult.text});
+      }
     });
 };
